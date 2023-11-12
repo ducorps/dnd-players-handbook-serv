454250b001ca44235a6b8105e4d9736e35022f1f
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-11-12 19:02:18 -0300

    added missing annotation

--- src/main/java/com/handbook/handbookapi/config/WebMvcConfig.java
+++ src/main/java/com/handbook/handbookapi/config/WebMvcConfig.java
@@ -2,9 +2,11 @@ package com.handbook.handbookapi.config;
 
 import com.handbook.handbookapi.security.RateLimitInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private final RateLimitInterceptor rateLimitInterceptor;
 
