3166d99b5c86ea054cc478152b1772b9f5b44ec3
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-11-12 18:41:13 -0300

    added beans to the application

--- src/main/java/com/handbook/handbookapi/HandbookapiApplication.java
+++ src/main/java/com/handbook/handbookapi/HandbookapiApplication.java
@@ -2,6 +2,13 @@ package com.handbook.handbookapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class HandbookapiApplication {
@@ -9,4 +16,25 @@ public class HandbookapiApplication {
 		SpringApplication.run(HandbookapiApplication.class, args);
 	}
 
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(jedisConnectionFactory());
+
+		return template;
+	}
+
+	@Bean
+	public JedisConnectionFactory jedisConnectionFactory() {
+		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+		redisConfig.setHostName("rediss://red-cl8iffqvokcc73b03r6g:Lb7P4yZDIrqB24ZzMqdMtX9COgNA9l8O@oregon-redis.render.com:6379");
+		redisConfig.setPort(6379);
+		redisConfig.setPassword("Lb7P4yZDIrqB24ZzMqdMtX9COgNA9l8O");
+		redisConfig.setUsername("red-cl8iffqvokcc73b03r6g");
+		JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfig = JedisClientConfiguration.builder();
+		jedisClientConfig.connectTimeout(Duration.ofSeconds(60));
+		jedisClientConfig.useSsl();
+
+		return new JedisConnectionFactory(redisConfig, jedisClientConfig.build());
+	}
 }
