e5c431cbffcf93ae5dacf373b89105c6e209cc65
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-11-12 18:42:23 -0300

    added the rate limit interceptor to the registry

---
+++ src/main/java/com/handbook/handbookapi/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.handbook.handbookapi.config;
+
+import com.handbook.handbookapi.security.RateLimitInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final RateLimitInterceptor rateLimitInterceptor;
+
+    @Autowired
+    public WebMvcConfig(RateLimitInterceptor rateLimitInterceptor) {
+        this.rateLimitInterceptor = rateLimitInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor);
+    }
+}
