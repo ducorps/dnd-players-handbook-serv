f3d0f6207ef6850a54806bd80e985e3523f4fd8b
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-11-13 22:59:34 -0300

    change path names to rest pattern

--- src/main/java/com/handbook/handbookapi/character/CharacterController.java
+++ src/main/java/com/handbook/handbookapi/character/CharacterController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 @RestController
-@RequestMapping("/api/characters")
+@RequestMapping("/characters")
 public class CharacterController {
     @Autowired
     private CharacterService service;
--- src/main/java/com/handbook/handbookapi/inventory/InventoryController.java
+++ src/main/java/com/handbook/handbookapi/inventory/InventoryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/inventory")
+@RequestMapping("/inventories")
 public class InventoryController {
     @Autowired
     private InventoryService inventoryService;
