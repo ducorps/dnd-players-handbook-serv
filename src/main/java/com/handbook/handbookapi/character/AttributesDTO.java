b5df4b8be573734d99ffc2645b67959436f2bfdd
Bruno-Venturini <brunofreloventurini@outlook.com>
2023-11-27 02:10:22 -0300

    ajustes step 4

---
+++ src/main/java/com/handbook/handbookapi/character/AttributesDTO.java
@@ -0,0 +1,12 @@
+package com.handbook.handbookapi.character;
+
+import javax.persistence.Column;
+
+public class AttributesDTO {
+    private Integer intelligence;
+    private Integer strength;
+    private Integer constitution;
+    private Integer wisdom;
+    private Integer dexterity;
+    private Integer charisma;
+}
