e555677e6a8f7689d1c4a4a28ff2e6bc2cfcc8fa
Sofia <sofia.martinslv@gmail.com>
2023-11-27 15:38:31 -0300

    update item

--- src/main/java/com/handbook/handbookapi/inventory/item/ItemController.java
+++ src/main/java/com/handbook/handbookapi/inventory/item/ItemController.java
@@ -2,10 +2,7 @@ package com.handbook.handbookapi.inventory.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,7 +20,7 @@ public class ItemController {
     }
 
     @DeleteMapping("/{idItem}")
-    public ResponseEntity deleteItem(Long idItem) {
+    public ResponseEntity deleteItem(@PathVariable("idItem") Long idItem) {
         itemService.delete(idItem);
 
         return ResponseEntity.ok().build();
